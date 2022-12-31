b3348722c38c74abfc3ae1c7bd6668adb2fad3fc
Jose Duran <joselduran@outlook.com>
2022-12-31 18:56:10 -0300

    Cargar Imagen

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -2,22 +2,26 @@ import { HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Project } from 'src/app/models/project';
 import { ProjectService } from 'src/app/services/project.service'; 
+import { UploadService } from 'src/app/services/upload.service';
+import { Global } from 'src/app/services/global';
 
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css'],
-  providers : [ProjectService]
+  providers : [ProjectService, UploadService]
 })
 export class CreateComponent  {
 
     public title : string;
     public project : Project;
     public status : string = "";
+    public filesToUpload : Array<File> = [] ;
 
     constructor(
-      private _projectService : ProjectService
+      private _projectService : ProjectService,
+      private _uploadService : UploadService
     ){
       this.title = " Crear Projecto ";
       this.project = new Project('','','','','',2023,'');
@@ -29,8 +33,18 @@ export class CreateComponent  {
           response =>{
             console.log(response);
             if(response.project){
-                this.status = "success";
-                form.reset();
+                
+                
+                // Subir la Imagen
+                this._uploadService.makeFileRequest(Global.url+"upload-image/"+response.project._id,[],this.filesToUpload, 'image')
+                .then((result:any) => {
+                  this.status = "success";
+                    console.log(result);
+                    form.reset();
+                });
+
+
+                
             }else{
               this.status = "failed";
             }
@@ -40,6 +54,11 @@ export class CreateComponent  {
           }
           
       );
+
+    }
+
+    fileChangeEvent(fileInput:any){
+      this.filesToUpload = <Array<File>>fileInput.target.files;
 
     }
 
