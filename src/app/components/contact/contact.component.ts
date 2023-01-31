868201cff7a80ac64ed77f5617668cec4a7a1ad8
Jose Duran <joselduran@outlook.com>
2023-01-31 12:27:18 -0300

    correcto funcinamiento

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+//import * as $ from 'jquery'
+declare var $: any;
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
 
+  ngOnInit(): void {
+    $("#logo").on("click", function(e: { preventDefault: () => void; }){
+      e.preventDefault();
+
+      $("header").css("background","green")
+                 .css("height","50px");
+
+    });
+
+    $("#texto").on("click", function(){
+      $("#texto").css("background", "red")
+    });
+
+    // SLIDER JQUERY 
+
+    $('.galeria').bxSlider({
+      mode: 'fade',
+      captions: true,
+      slideWidth: 800
+    });
+    
+  }
 }
